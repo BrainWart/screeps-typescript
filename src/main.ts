f146c8a2f30946444a13c3c05d3438e345e24a4a
Cody McGinnis <brainwart@gmail.com>
2020-04-20 14:34:55 -0400

    back to nothing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { JobManager } from "managers/JobManager";
-import { Manager } from "managers/Manager";
-import { RoomManager } from "managers/RoomManager";
+import { ScreepsPrometheus } from "@brainwart/screeps-prometheus-game";
 import { ErrorMapper } from "utils/ErrorMapper";
 import { Logger, LogLevel } from "utils/Logger";
 import { Timer } from "utils/Timer";
@@ -19,29 +17,50 @@ const logger = new Logger("MAIN");
 
 logger.logError(`START - ${pack.name} - ${pack.version}`);
 
-if (Memory.version.major !== version.major || Memory.version.branch === "dev") {
-  logger.logWarning("Clearing memory");
-  for (const index in Memory) {
-    delete Memory[index];
-  }
+// if (Memory.version.major !== version.major || Memory.version.branch === "dev") {
+//   logger.logWarning("Clearing memory");
+//   for (const index in Memory) {
+//     delete Memory[index];
+//   }
 
-  logger.logTrace1("Setting built-in memory to objects");
-  Memory.version = version;
-  Memory.creeps = {};
-  Memory.flags = {};
-  Memory.powerCreeps = {};
-  Memory.rooms = {};
-  Memory.spawns = {};
-}
+//   logger.logTrace1("Setting built-in memory to objects");
+//   Memory.version = version;
+//   Memory.creeps = {};
+//   Memory.flags = {};
+//   Memory.powerCreeps = {};
+//   Memory.rooms = {};
+//   Memory.spawns = {};
+// }
 
 export const loop = ErrorMapper.wrapLoop(() =>
   Timer.log(logger, () => {
+    const prom = new ScreepsPrometheus();
+
+    const cpu = prom.addPrefix("cpu");
+    cpu.addGauge("used", Game.cpu.getUsed());
+    cpu.addGauge("bucket", Game.cpu.bucket);
+
+    const rooms = prom.addPrefix("room");
+
     for (const roomName in Game.rooms) {
       const room = Game.rooms[roomName];
 
-      const roomManager = Manager.GetManager(RoomManager, room);
-      const jobManager = Manager.GetManager(JobManager, room);
-      jobManager.CheckJobs();
+      if (room.controller && room.controller.my) {
+        const roomSummary = rooms.addLabel("roomName", roomName);
+
+        const controller = roomSummary.addPrefix("controller");
+        controller.addGauge("level", room.controller.level).addHelp("Current controller level");
+        controller.addGauge("progress", room.controller.progress);
+        controller.addGauge("progressNeeded", room.controller.progressTotal);
+        controller.addGauge("downgrade", room.controller.ticksToDowngrade);
+
+        if (room.storage) {
+          const storage = roomSummary.addPrefix("storage");
+          storage.addGauge("energy", 20);
+        }
+      }
     }
+
+    Memory.stats = prom.build();
   })
 );
