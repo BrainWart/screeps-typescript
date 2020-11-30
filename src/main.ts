b6ea4336cc73353d9db9d3ccfd5ef514c06ce8cb
Cody McGinnis <brainwart@gmail.com>
2020-11-30 05:04:56 -0500

    add shard to global logger data

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { Logger } from "utils/logging/Logger";
 import { Timer } from "utils/Timer";
 import { Version } from "utils/Version";
 
-const logger = new Logger("MAIN", [ new FancyScreepsConsoleProvider() ]);
+const logger = new Logger("MAIN", [ new FancyScreepsConsoleProvider() ], { shard: Game.shard.name });
 
 logger.logAlert(`START - ${Version.name} - ${Version.string} - ${Game.shard.name}`);
 
 export const loop = ErrorMapper.wrapLoop(() => Timer.log(logger, () => {
   for (const roomName in Game.rooms) {
-    const localLogger = logger.scoped(roomName, { shard: Game.shard.name, room: roomName });
+    const localLogger = logger.scoped(roomName, { room: roomName });
     localLogger.logInfo("logging for room " + roomName);
   }
 }));
