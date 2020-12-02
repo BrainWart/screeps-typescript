c81f512725ed13c4dd9c7e68a870730371e9dadb
Cody McGinnis <brainwart@gmail.com>
2020-12-01 20:37:19 -0500

    2 spawners in worker limit

--- src/main.ts
+++ src/main.ts
@@ -109,7 +109,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
             // tslint:disable: object-literal-sort-keys
             // prettier-ignore
             const workerLimits: Record<Tasks, number> = {
-              spawn: 1,
+              spawn: 2,
               harvest: room.memory.harvestables.length,
               attack: room.find(FIND_HOSTILE_CREEPS).length > 0 ? 1 : 0,
               build:
